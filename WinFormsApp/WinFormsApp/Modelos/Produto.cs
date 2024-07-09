478b5724656a43953915490c198081d8e7ab86ff
LoboGamingg <pablorenan12@outlook.com>
2024-07-09 15:26:11 -0300

    Criacao do basico, foi instalado os pacotes do frameworkCore, criada uma tabela(modelo) e criado o esboço do bancoContext

---
+++ WinFormsApp/WinFormsApp/Modelos/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp.Itens
+{
+    internal class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idProduto { get; set; }
+        public string nomeProduto { get; set; }  = string.Empty;
+    }
+}
